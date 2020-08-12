151a0782affc073695f22acf2ce1eecab586bf5e
William Cleghorn <wcleg4@pop-os.localdomain>
2020-08-11 18:41:03 -0600

    1 & 2

---
+++ its_elemental.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {	
+    printf("H\n");
+    printf("He\n");
+    printf("Li\n");
+    printf("Be\n");
+    printf("B\n");
+    printf("C\n");
+    printf("N\n");
+    printf("O\n");
+    printf("F\n");
+    printf("Ne\n");
+    return 0;
+}
