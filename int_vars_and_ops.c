5c87ad3482685f11d94613503ba240cea94a8acd
William Cleghorn <wcleg4@pop-os.localdomain>
2020-08-11 18:55:24 -0600

    completed int vars and ops

--- int_vars_and_ops.c
+++ int_vars_and_ops.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-    printf()
+
+    int youngAge = 2020 - 2002;
+    printf("%d\n", youngAge);
+
+    int oldAge = 2020 - 1962;
+    printf("%d\n", oldAge);
+
+    int ageDifference = oldAge - youngAge;
+    printf("%d\n", ageDifference);
+
+    return 0;
 }
