f8d195856dd5960c4705a1490524bac85eba458f
William Cleghorn <wcleg4-work@pop-os.localdomain>
2020-10-30 17:50:13 -0600

    junior code breaker

---
+++ junior_codebreaker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+
+    printf("Welcome to Junior Codebreaker!\n");
+    printf("\nThis Program will output the\noccurences of each letter in a phrase\n");
+    printf("Enter a phrase (no more than 99 characters): ");
+    
+    char phrase[99] = {0};
+    fgets(phrase, 99, stdin);
+
+    // Strips newline
+    phrase[strcspn(phrase, "\n")] = 0;
+
+    printf("\nPhrase: %s\n\n", phrase);
+    
+    int occurences[26] = {0};
+
+
+    for (int i=0; i<=99; i++) {
+        if (isalpha(phrase[i])) {
+            occurences[(int) toupper(phrase[i]) - 65] += 1;
+        }
+    }
+
+    for (int i=0; i<26; i++) {
+        if (occurences[i] != 0) {
+            printf("%c appears %d times\n", (char) i+65, occurences[i]);
+        }
+    }
+}
